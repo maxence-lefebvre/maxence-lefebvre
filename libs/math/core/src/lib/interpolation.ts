854b427ad8383b7103974790cc85568abed4fe6d
mlefebvre-amalia <maxence@amalia.io>
2024-01-02 19:00:44 +0100

    refactor: remove dead code

--- libs/math/core/src/lib/interpolation.ts
+++ libs/math/core/src/lib/interpolation.ts
@@ -1,16 +1,2 @@
 export const linearInterpolation = (a: number, b: number, t: number) =>
   a + t * (b - a);
-
-export type PointLike = {
-  x: number;
-  y: number;
-};
-
-export const linearInterpolation2D = (
-  a: PointLike,
-  b: PointLike,
-  t: number,
-) => ({
-  x: linearInterpolation(a.x, b.x, t),
-  y: linearInterpolation(a.y, b.y, t),
-});
