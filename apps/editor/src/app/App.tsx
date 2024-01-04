1df72c6fb5debe57a7e9e5289bbe1b2b84cdd5a8
mlefebvre-amalia <maxence@amalia.io>
2024-01-04 09:07:18 +0100

    feat: better control bar

--- apps/editor/src/app/App.tsx
+++ apps/editor/src/app/App.tsx
@@ -33,11 +33,18 @@ export const App = memo(function App() {
         css={css`
           position: absolute;
           top: 20px;
-          right: 20px;
+          left: 50%;
+          transform: translateX(-50%);
 
-          flex-direction: column;
-          background-color: rgba(0, 0, 0, 0.5);
-          padding: 10px;
+          padding: 0.25rem;
+          background-color: #fff;
+          border-radius: 0.5rem;
+          box-shadow:
+            0 0 1px 0 rgba(0, 0, 0, 0.17),
+            0 0 3px 0 rgba(0, 0, 0, 0.08),
+            0 7px 14px 0 rgba(0, 0, 0, 0.05);
+
+          transition: box-shadow 0.5s ease-in-out;
         `}
       />
     </AppContainer>
--- libs/canvas/components/src/lib/CanvasControls.tsx
+++ libs/canvas/components/src/lib/CanvasControls.tsx
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { useViewportContext } from '@feyroads/editor/viewport/state';
 import { useKeyboard } from '@feyroads/ext/react/hooks';
 import { useDebugModeContext } from '@feyroads/kernel/debug';
@@ -6,8 +7,59 @@ import { useGraphStateContext } from '@feyroads/math/state';
 import { IconDeviceFloppy, IconTool, IconTrash } from '@tabler/icons-react';
 import { ComponentPropsWithoutRef, memo, useCallback } from 'react';
 
-
 export type CanvasControlsProps = ComponentPropsWithoutRef<'div'>;
+
+export const ControlButton = styled.button`
+  position: relative;
+
+  all: unset;
+
+  --border-radius: 0.25rem;
+  --size: 2.5rem;
+
+  --hover-bg-color: #e5e4ff;
+  --icon-color: #1b1b1f;
+  --hover-color: #4946bd;
+
+  border-radius: var(--border-radius);
+  display: inline-flex;
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--hover-bg-color);
+
+    * {
+      color: var(--hover-color);
+    }
+  }
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border-radius: var(--border-radius);
+  width: var(--size);
+  height: var(--size);
+`;
+
+export const Divider = styled.div`
+  width: 1px;
+  height: 1.5rem;
+
+  align-self: center;
+
+  background-color: #8484b9;
+
+  margin: 0 0.25rem;
+`;
+
+export const icon = css`
+  width: calc(var(--size) / 2);
+  height: calc(var(--size) / 2);
+  color: var(--icon-color);
+`;
 
 export const CanvasControls = memo(function CanvasControls({
   ...props
@@ -27,19 +79,36 @@ export const CanvasControls = memo(function CanvasControls({
     <div
       {...props}
       css={css`
-        display: flex;
-        gap: 20px;
+        display: grid;
+        grid-auto-flow: column;
+        grid-auto-columns: min-content;
+        grid-template-rows: auto;
+
+        gap: 0.25rem;
       `}
     >
-      <button onClick={onClickSave} title="Save graph to local storage">
-        <IconDeviceFloppy />
-      </button>
-      <button onClick={disposeGraph} title="Reset graph to default">
-        <IconTrash />
-      </button>
-      <button onClick={toggleDebugMode} title="Toggle debug mode">
-        <IconTool />
-      </button>
+      <ControlButton
+        onClick={onClickSave}
+        title="Save graph to local storage -- ctrl + s"
+      >
+        <IconContainer>
+          <IconDeviceFloppy css={icon} />
+        </IconContainer>
+      </ControlButton>
+      <ControlButton onClick={disposeGraph} title="Reset graph to default">
+        <IconContainer>
+          <IconTrash css={icon} />
+        </IconContainer>
+      </ControlButton>
+      <Divider />
+      <ControlButton
+        onClick={toggleDebugMode}
+        title="Toggle debug mode -- ctrl + d"
+      >
+        <IconContainer>
+          <IconTool css={icon} />
+        </IconContainer>
+      </ControlButton>
     </div>
   );
 });
